fb89c7c6f96fa6c19424a9b4ed54bffcfca8d5fa
valyalshikovd <770vda@mail.ru>
2025-04-17 09:23:54 +0300

    fix: remove incorrect models

--- BackendJava/UserService/src/main/java/backend/academy/userservice/repository/RoleRepository.java
+++ BackendJava/UserService/src/main/java/backend/academy/userservice/repository/RoleRepository.java
@@ -1,6 +1,5 @@
 package backend.academy.userservice.repository;
 
-import backend.academy.userservice.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- BackendJava/UserService/src/main/java/backend/academy/userservice/servcie/RoleService.java
+++ BackendJava/UserService/src/main/java/backend/academy/userservice/servcie/RoleService.java
@@ -1,6 +1,5 @@
 package backend.academy.userservice.servcie;
 
-import backend.academy.userservice.model.Role;
 import backend.academy.userservice.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
