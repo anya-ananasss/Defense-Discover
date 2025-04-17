d81690a0cbe150a44454d128122c87c4239fdf8d
valyalshikovd <770vda@mail.ru>
2025-04-17 09:29:29 +0300

    add: add RoleRepository

--- BackendJava/UserService/src/main/java/backend/academy/userservice/repository/RoleRepository.java
+++ BackendJava/UserService/src/main/java/backend/academy/userservice/repository/RoleRepository.java
@@ -1,8 +1,9 @@
 package backend.academy.userservice.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import backend.academy.userservice.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRolename(String role_name);
 }
