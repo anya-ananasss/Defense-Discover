baa4940c7d4a466c2f068161cb69848344bb2533
valyalshikovd <770vda@mail.ru>
2025-04-17 11:52:31 +0300

    fix: fix RoleMapper

--- BackendJava/UserService/src/main/java/backend/academy/userservice/mapper/RoleMapper.java
+++ BackendJava/UserService/src/main/java/backend/academy/userservice/mapper/RoleMapper.java
@@ -1,7 +1,18 @@
 package backend.academy.userservice.mapper;
 
+import backend.academy.userservice.dto.RoleDto;
+import backend.academy.userservice.model.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface RoleMapper {
+
+
+    RoleMapper INSTANCE = Mappers.getMapper(RoleMapper.class);
+
+    RoleDto roleToRoleDTO(Role role);
+
+    Role roleDTOToRole(RoleDto roleDTO);
+
 }
