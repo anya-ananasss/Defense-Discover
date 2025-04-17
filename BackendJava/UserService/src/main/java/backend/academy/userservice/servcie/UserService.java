3891d4a6104f2b0e3223fd7c90743860355291b2
valyalshikovd <770vda@mail.ru>
2025-04-17 12:15:34 +0300

    add: add UserService

--- BackendJava/UserService/src/main/java/backend/academy/userservice/servcie/UserService.java
+++ BackendJava/UserService/src/main/java/backend/academy/userservice/servcie/UserService.java
@@ -2,15 +2,16 @@ package backend.academy.userservice.servcie;
 
 import backend.academy.common.dto.CreateDefaultUserRequestDto;
 import backend.academy.userservice.dto.UserDto;
+import backend.academy.userservice.exception.UserNotFoundException;
 
 import java.util.List;
 
 public interface UserService {
 
     UserDto createDefaultUser(CreateDefaultUserRequestDto createDefaultUserRequestDto);
-    UserDto changeUserName(String newName);
-    String getPassword(String username);
-    String removeUser(String username);
+    UserDto changeUserName(String oldName, String newName) throws UserNotFoundException;
+    String getPassword(String username) throws UserNotFoundException;
+    String removeUser(String username) throws UserNotFoundException;
     UserDto createAdminUser(CreateDefaultUserRequestDto createDefaultUserRequestDto);
     List<UserDto> getAllUsers();
 }
--- BackendJava/UserService/src/main/java/backend/academy/userservice/servcie/impl/UserServiceImpl.java
+++ BackendJava/UserService/src/main/java/backend/academy/userservice/servcie/impl/UserServiceImpl.java
@@ -2,13 +2,19 @@ package backend.academy.userservice.servcie.impl;
 
 import backend.academy.common.dto.CreateDefaultUserRequestDto;
 import backend.academy.userservice.dto.UserDto;
+import backend.academy.userservice.exception.UserNotFoundException;
+import backend.academy.userservice.mapper.UserMapper;
+import backend.academy.userservice.model.Role;
 import backend.academy.userservice.model.User;
+import backend.academy.userservice.repository.RoleRepository;
 import backend.academy.userservice.repository.UserRepository;
 import backend.academy.userservice.servcie.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -16,36 +22,70 @@ public class UserServiceImpl implements UserService {
 
 
     private final UserRepository userRepository;
-
+    private final RoleRepository roleRepository;
+    private final UserMapper userMapper;
 
     @Override
     public UserDto createDefaultUser(CreateDefaultUserRequestDto createDefaultUserRequestDto) {
-        return null;
+        return createUser(createDefaultUserRequestDto, Set.of(roleRepository.findByRolename("user")));
     }
 
     @Override
-    public UserDto changeUserName(String newName) {
-        return null;
+    public UserDto changeUserName(String oldName, String newName) throws UserNotFoundException {
+        User userEntity = getUserByUsername(oldName);
+        userEntity.setUsername(newName);
+        return userMapper.userToUserDTO(userRepository.save(userEntity));
     }
 
     @Override
-    public String getPassword(String username) {
-        return "";
+    public String getPassword(String username) throws UserNotFoundException {
+        User userEntity = getUserByUsername(username);
+        return userEntity.getPassword();
     }
 
     @Override
-    public String removeUser(String username) {
-        return "";
+    public String removeUser(String username) throws UserNotFoundException {
+        User userEntity = getUserByUsername(username);
+        userRepository.delete(userEntity);
     }
 
     @Override
     public UserDto createAdminUser(CreateDefaultUserRequestDto createDefaultUserRequestDto) {
-        return null;
+       return createUser(createDefaultUserRequestDto, Set.of(roleRepository.findByRolename("admin")));
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return List.of();
+        return userRepository
+                .findAll()
+                .stream()
+                .map(userMapper::userToUserDTO)
+                .toList();
+    }
+
+    private UserDto createUser(CreateDefaultUserRequestDto createDefaultUserRequestDto, Set<Role> roles) {
+        User userEntity = User
+                .builder()
+                .roles(
+                        roles
+                )
+                .username(createDefaultUserRequestDto.username())
+                .email(createDefaultUserRequestDto.mail())
+                .password(createDefaultUserRequestDto.password())
+                .keyWords(Set.of())
+                .stats(Set.of())
+                .build();
+
+        return userMapper.userToUserDTO(userRepository.save(userEntity));
+    }
+
+    private User getUserByUsername(String username) throws UserNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if(optionalUser.isEmpty()) {
+            throw new UserNotFoundException("User not found");
+        }
+
+        return optionalUser.get();
     }
 }
 
