95c429861e435de54af3364b90066c0ffe1a3021
SoniaFilozop <72881848+SoniaFilozop@users.noreply.github.com>
2025-04-18 11:21:45 +0300

    upgrade prompts.py

--- llm_config/prompts.py
+++ llm_config/prompts.py
@@ -3,5 +3,5 @@ from langchain.prompts import PromptTemplate
 quiz_prompt = PromptTemplate(
     input_variables=["topic", "num_questions", "difficulty"],
     template="""Придумай {num_questions} {difficulty} неповторяющихся вопроса для викторины по теме {topic}. 
-    Выведи результат в виде JSON с полями: question (вопрос), options (4 варианта ответов) и correct_answer (правильный ответ).""",
+    Выведи результат в виде JSON с полями: topic (тема), question (вопрос), options (4 варианта ответов), correct_answer (правильный ответ) и key_words (ключевые слова).""",
 )
