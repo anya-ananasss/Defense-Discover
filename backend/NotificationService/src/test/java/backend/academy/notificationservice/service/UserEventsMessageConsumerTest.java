9dc61521a9c08f6f655369f364b5504804b3eeb8
Gureva Anna Mikhailovna <149677477+anya-ananasss@users.noreply.github.com>
2025-06-02 18:21:27 +0300

    fix: fix unit-tests

--- backend/NotificationService/src/test/java/backend/academy/notificationservice/service/UserEventsMessageConsumerTest.java
+++ backend/NotificationService/src/test/java/backend/academy/notificationservice/service/UserEventsMessageConsumerTest.java
@@ -10,11 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,9 +23,6 @@ class UserEventsMessageConsumerTest {
 
     @Mock
     private MessageService messageService;
-
-    @Mock
-    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Mock
     private Acknowledgment acknowledgment;
@@ -43,13 +38,10 @@ class UserEventsMessageConsumerTest {
         String message = objectMapper.writeValueAsString(confirmation);
         ConsumerRecord<Long, String> record = new ConsumerRecord<>("topic", 0, 0L, 1L, message);
 
-
         consumer.consume(record, acknowledgment);
-
 
         verify(messageService).sendMail(any(UserConfirmation.class));
         verify(acknowledgment).acknowledge();
-        verify(kafkaTemplate, never()).send(anyString(), anyString());
     }
 
     @Test
@@ -60,25 +52,10 @@ class UserEventsMessageConsumerTest {
         String message = objectMapper.writeValueAsString(repair);
         ConsumerRecord<Long, String> record = new ConsumerRecord<>("topic", 0, 0L, 1L, message);
 
-
         consumer.consumeRepairPassword(record, acknowledgment);
-
 
         verify(messageService).sendMailRepairPassword(any(PasswordRepair.class));
         verify(acknowledgment).acknowledge();
-        verify(kafkaTemplate, never()).send(anyString(), anyString());
     }
 
-    @Test
-    void shouldHandleInvalidMessage() {
-        String invalidMessage = "invalid json";
-        ConsumerRecord<Long, String> record = new ConsumerRecord<>("topic", 0, 0L, 1L, invalidMessage);
-        when(kafkaTemplate.send(anyString(), anyString())).thenReturn(null);
-
-        consumer.consume(record, acknowledgment);
-
-        verify(messageService, never()).sendMail(any(UserConfirmation.class));
-        verify(acknowledgment).acknowledge();
-        verify(kafkaTemplate).send(endsWith("-dlt"), anyString());
-    }
 }
